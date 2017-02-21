1314b625d09e8263e1503aeefc1ef55dfdd59683
dsartain18 <dsartain@sartainsoftwaresolutions.com>
2017-02-20 21:16:45 -0600

    Added ToDo List demo project

---
+++ ToDoList/ToDoList/Scripts/listService.js
@@ -0,0 +1,41 @@
+﻿(function () {
+
+    var listService = function () {
+
+        var getListItems = function () {
+
+            var listItems = [];
+
+            var listItem = {
+                "ItemDescription": "Example of completed task",
+                "IsComplete": true
+            };
+
+            var listItem2 = {
+                "ItemDescription": "Example of incomplete task",
+                "IsComplete": false
+            };
+
+            var listItem3 = {
+                "ItemDescription": "This is an example of a really long incomplete task",
+                "IsComplete": false
+            }
+
+            listItems.push(listItem);
+            listItems.push(listItem2);
+            listItems.push(listItem3);
+
+            return listItems;
+
+        };
+
+        return {
+            getListItems: getListItems
+        };
+
+    };
+
+    var module = angular.module("todolist");
+    module.factory("listService", listService);
+
+}());
---
+++ ToDoList/ToDoList/Scripts/maincontroller.js
@@ -0,0 +1,42 @@
+﻿(function () {
+    var app = angular.module("todolist", []);
+
+    var MainController = function ($scope, listService) {
+
+        var onListComplete = function (response) {
+            $scope.listItems = response.data;
+
+            console.log("List Successfully Retrieved");
+            console.log(response.data);
+        };
+
+        var onError = function (reason) {
+            console.log("There was an error");
+            console.log(reason);
+        };
+
+        $scope.getListItems = function () {
+
+            $scope.listItems = listService.getListItems();
+        };
+
+        $scope.addItem = function (itemDescription) {
+
+            var listItem = {
+                "ItemDescription": itemDescription,
+                "IsComplete": false
+            };
+
+            $scope.listItems.push(listItem);
+
+            $scope.itemDescription = null;
+        };
+
+        $scope.message = "To Do List Demo";
+
+        $scope.getListItems();
+    };
+
+
+    app.controller("MainController", ["$scope", "listService", MainController]);
+}());
